14e34b0a67cbaf9b701c4168bac25f57fb9adc9c
Ryan Ogden <12yanogden@gmail.com>
2024-09-30 15:16:30 -0400

    add isOfReflectKind

--- any.go
+++ any.go
@@ -4,8 +4,9 @@ import (
 	"reflect"
 )
 
-func IsBasic(i any) bool {
-	switch reflect.TypeOf(i).Kind() {
+// Return true if the value given is a basic type. Else, false.
+func IsBasic(v any) bool {
+	switch reflect.TypeOf(v).Kind() {
 	case reflect.Bool,
 		reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
 		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
@@ -18,12 +19,14 @@ func IsBasic(i any) bool {
 	}
 }
 
-func IsComparable(i any) bool {
-	return reflect.ValueOf(i).Comparable()
+// Return true if the value given is comparable. Else, false.
+func IsComparable(v any) bool {
+	return reflect.ValueOf(v).Comparable()
 }
 
-func IsOrdered(i any) bool {
-	t := reflect.TypeOf(i)
+// Return true if the value given adheres to cmp.Ordered. Else, false.
+func IsOrdered(v any) bool {
+	t := reflect.TypeOf(v)
 	switch t.Kind() {
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
 		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr,
@@ -32,4 +35,29 @@ func IsOrdered(i any) bool {
 	default:
 		return false
 	}
+}
+
+// Return true if the value given is a map. Else, false.
+func IsMap(v any) bool {
+	return isOfReflectKind(v, reflect.Map)
+}
+
+// Return true if the value given is a slice. Else, false.
+func IsSlice(v any) bool {
+	return isOfReflectKind(v, reflect.Slice)
+}
+
+// Return true if the value given is an array. Else, false.
+func IsArray(v any) bool {
+	return isOfReflectKind(v, reflect.Array)
+}
+
+// Return true if the value given is a struct. Else, false.
+func IsStruct(v any) bool {
+	return isOfReflectKind(v, reflect.Struct)
+}
+
+// Return true if the value given has a reflect type kind that matches the kind given. Else, false.
+func isOfReflectKind(v any, kind reflect.Kind) bool {
+	return reflect.TypeOf(v).Kind() == kind
 }
--- any_test.go
+++ any_test.go
@@ -193,3 +193,278 @@ func TestIsNotOrdered(t *testing.T) {
 		}
 	}
 }
+
+func TestIsMap(t *testing.T) {
+	typedValues := []any{
+		map[any]any{},       // any, any map
+		map[int]any{},       // int, any map
+		map[string]any{},    // string, any map
+		map[any]int{},       // any, int map
+		map[int]int{},       // int, int map
+		map[string]int{},    // string, int map
+		map[any]string{},    // any, string map
+		map[int]string{},    // int, string map
+		map[string]string{}, // string, string map
+	}
+	expected := []bool{}
+	actual := []bool{}
+
+	// Set expected and actual values
+	for _, v := range typedValues {
+		expected = append(expected, true)
+		actual = append(actual, IsMap(v))
+	}
+
+	// Test actual values against expected
+	for i := 0; i < len(typedValues); i++ {
+		if actual[i] != expected[i] {
+			valueType := reflect.TypeOf(typedValues[i])
+			t.Fatalf("\nExpected:\t%t, %s is a map\nActual:\t\t%t, %s is NOT a map\n", expected[i], valueType, actual[i], valueType)
+		}
+	}
+}
+
+func TestIsNotMap(t *testing.T) {
+	typedValues := []any{
+		true,                     // bool
+		int(1),                   // int
+		int8(2),                  // int8
+		int16(3),                 // int16
+		int32(4),                 // int32
+		int64(5),                 // int64
+		uint(6),                  // uint
+		uint8(7),                 // uint8
+		uint16(8),                // uint16
+		uint32(9),                // uint32
+		uint64(10),               // uint64
+		float32(11.1),            // float32
+		float64(12.2),            // float64
+		complex64(13.3 + 14.4i),  // complex64
+		complex128(15.5 + 16.6i), // complex128
+		"hello",                  // string
+		MyStruct{},               // struct
+		[]any{},                  // slice
+		[1]any{"array"},          // array
+	}
+	expected := []bool{}
+	actual := []bool{}
+
+	// Set expected and actual values
+	for _, v := range typedValues {
+		expected = append(expected, false)
+		actual = append(actual, IsMap(v))
+	}
+
+	// Test actual values against expected
+	for i := 0; i < len(typedValues); i++ {
+		if actual[i] != expected[i] {
+			valueType := reflect.TypeOf(typedValues[i])
+			t.Fatalf("\nExpected:\t%t, %s is not a map\nActual:\t\t%t, %s IS a map\n", expected[i], valueType, actual[i], valueType)
+		}
+	}
+}
+
+func TestIsSlice(t *testing.T) {
+	typedValues := []any{
+		[]any{},      // any slice
+		[]int{},      // int slice
+		[]string{},   // string slice
+		[]MyStruct{}, // struct slice
+	}
+	expected := []bool{}
+	actual := []bool{}
+
+	// Set expected and actual values
+	for _, v := range typedValues {
+		expected = append(expected, true)
+		actual = append(actual, IsSlice(v))
+	}
+
+	// Test actual values against expected
+	for i := 0; i < len(typedValues); i++ {
+		if actual[i] != expected[i] {
+			valueType := reflect.TypeOf(typedValues[i])
+			t.Fatalf("\nExpected:\t%t, %s is a slice\nActual:\t\t%t, %s is NOT a slice\n", expected[i], valueType, actual[i], valueType)
+		}
+	}
+}
+
+func TestIsNotSlice(t *testing.T) {
+	typedValues := []any{
+		true,                     // bool
+		int(1),                   // int
+		int8(2),                  // int8
+		int16(3),                 // int16
+		int32(4),                 // int32
+		int64(5),                 // int64
+		uint(6),                  // uint
+		uint8(7),                 // uint8
+		uint16(8),                // uint16
+		uint32(9),                // uint32
+		uint64(10),               // uint64
+		float32(11.1),            // float32
+		float64(12.2),            // float64
+		complex64(13.3 + 14.4i),  // complex64
+		complex128(15.5 + 16.6i), // complex128
+		"hello",                  // string
+		MyStruct{},               // struct
+		map[any]any{},            // map
+		[1]any{"array"},          // array
+	}
+	expected := []bool{}
+	actual := []bool{}
+
+	// Set expected and actual values
+	for _, v := range typedValues {
+		expected = append(expected, false)
+		actual = append(actual, IsSlice(v))
+	}
+
+	// Test actual values against expected
+	for i := 0; i < len(typedValues); i++ {
+		if actual[i] != expected[i] {
+			valueType := reflect.TypeOf(typedValues[i])
+			t.Fatalf("\nExpected:\t%t, %s is not a slice\nActual:\t\t%t, %s IS a slice\n", expected[i], valueType, actual[i], valueType)
+		}
+	}
+}
+
+func TestIsArray(t *testing.T) {
+	typedValues := []any{
+		[1]any{"array"},             // any array
+		[1]string{"array"},          // string array
+		[1]int{1},                   // int array
+		[1]int8{2},                  // int8 array
+		[1]int16{3},                 // int16 array
+		[1]int32{4},                 // int32 array
+		[1]int64{5},                 // int64 array
+		[1]uint{6},                  // uint array
+		[1]uint8{7},                 // uint8 array
+		[1]uint16{8},                // uint16 array
+		[1]uint32{9},                // uint32 array
+		[1]uint64{10},               // uint64 array
+		[1]float32{11.1},            // float32 array
+		[1]float64{12.2},            // float64 array
+		[1]complex64{13.3 + 14.4i},  // complex64 array
+		[1]complex128{15.5 + 16.6i}, // complex128 array
+		[1]MyStruct{},               // struct array
+		[2]any{"array", "array"},    // multi-element array
+	}
+	expected := []bool{}
+	actual := []bool{}
+
+	// Set expected and actual values
+	for _, v := range typedValues {
+		expected = append(expected, true)
+		actual = append(actual, IsArray(v))
+	}
+
+	// Test actual values against expected
+	for i := 0; i < len(typedValues); i++ {
+		if actual[i] != expected[i] {
+			valueType := reflect.TypeOf(typedValues[i])
+			t.Fatalf("\nExpected:\t%t, %s is an array\nActual:\t\t%t, %s is NOT an array\n", expected[i], valueType, actual[i], valueType)
+		}
+	}
+}
+
+func TestIsNotArray(t *testing.T) {
+	typedValues := []any{
+		true,                     // bool
+		int(1),                   // int
+		int8(2),                  // int8
+		int16(3),                 // int16
+		int32(4),                 // int32
+		int64(5),                 // int64
+		uint(6),                  // uint
+		uint8(7),                 // uint8
+		uint16(8),                // uint16
+		uint32(9),                // uint32
+		uint64(10),               // uint64
+		float32(11.1),            // float32
+		float64(12.2),            // float64
+		complex64(13.3 + 14.4i),  // complex64
+		complex128(15.5 + 16.6i), // complex128
+		"hello",                  // string
+		MyStruct{},               // struct
+		map[any]any{},            // map
+	}
+	expected := []bool{}
+	actual := []bool{}
+
+	// Set expected and actual values
+	for _, v := range typedValues {
+		expected = append(expected, false)
+		actual = append(actual, IsArray(v))
+	}
+
+	// Test actual values against expected
+	for i := 0; i < len(typedValues); i++ {
+		if actual[i] != expected[i] {
+			valueType := reflect.TypeOf(typedValues[i])
+			t.Fatalf("\nExpected:\t%t, %s is not an array\nActual:\t\t%t, %s IS an array\n", expected[i], valueType, actual[i], valueType)
+		}
+	}
+}
+
+func TestIsStruct(t *testing.T) {
+	typedValues := []any{
+		MyStruct{}, // struct
+	}
+	expected := []bool{}
+	actual := []bool{}
+
+	// Set expected and actual values
+	for _, v := range typedValues {
+		expected = append(expected, true)
+		actual = append(actual, IsStruct(v))
+	}
+
+	// Test actual values against expected
+	for i := 0; i < len(typedValues); i++ {
+		if actual[i] != expected[i] {
+			valueType := reflect.TypeOf(typedValues[i])
+			t.Fatalf("\nExpected:\t%t, %s is a struct\nActual:\t\t%t, %s is NOT a struct\n", expected[i], valueType, actual[i], valueType)
+		}
+	}
+}
+
+func TestIsNotStruct(t *testing.T) {
+	typedValues := []any{
+		true,                     // bool
+		int(1),                   // int
+		int8(2),                  // int8
+		int16(3),                 // int16
+		int32(4),                 // int32
+		int64(5),                 // int64
+		uint(6),                  // uint
+		uint8(7),                 // uint8
+		uint16(8),                // uint16
+		uint32(9),                // uint32
+		uint64(10),               // uint64
+		float32(11.1),            // float32
+		float64(12.2),            // float64
+		complex64(13.3 + 14.4i),  // complex64
+		complex128(15.5 + 16.6i), // complex128
+		"hello",                  // string
+		[]any{},                  // slice
+		[1]any{"array"},           // array
+		map[any]any{},            // map
+	}
+	expected := []bool{}
+	actual := []bool{}
+
+	// Set expected and actual values
+	for _, v := range typedValues {
+		expected = append(expected, false)
+		actual = append(actual, IsStruct(v))
+	}
+
+	// Test actual values against expected
+	for i := 0; i < len(typedValues); i++ {
+		if actual[i] != expected[i] {
+			valueType := reflect.TypeOf(typedValues[i])
+			t.Fatalf("\nExpected:\t%t, %s is not a struct\nActual:\t\t%t, %s IS a struct\n", expected[i], valueType, actual[i], valueType)
+		}
+	}
+}
